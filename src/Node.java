5158ffe658b25e0062622367551f1b30cf675b2b
SamuelVelasquezB <samuel.velasquezb@udea.edu.co>
2026-05-06 07:54:49 -0500

    Update Node.java

--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node {
+
+    List<String> keys;
+    List<Node> children;
+    boolean isLeaf;
+
+   
+    Node(boolean isLeaf) {
+        this.isLeaf = isLeaf;
+        this.keys = new ArrayList<>();
+        this.children = new ArrayList<>();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < keys.size(); i++) {
+            sb.append(keys.get(i));
+            if (i < keys.size() - 1) sb.append(" | ");
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
