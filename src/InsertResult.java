9356dc34f44a3311e3976a6073ff0531298289af
SamuelVelasquezB <samuel.velasquezb@udea.edu.co>
2026-05-06 07:56:23 -0500

    Update InsertResult.java

--- src/InsertResult.java
+++ src/InsertResult.java
@@ -1,2 +1,13 @@
 public class InsertResult {
+    boolean overflow = false;
+    SplitResult splitResult = null;
+
+    /** Resultado sin overflow. */
+    InsertResult() {}
+
+    /** Resultado con overflow: se debe propagar la división. */
+    InsertResult(SplitResult splitResult) {
+        this.overflow = true;
+        this.splitResult = splitResult;
+    }
 }
