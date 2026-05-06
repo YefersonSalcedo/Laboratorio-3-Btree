ee44bf99ad81457520b946bbd58d16a847bfaa5e
SamuelVelasquezB <samuel.velasquezb@udea.edu.co>
2026-05-06 07:55:59 -0500

    Update SplitResult.java

--- src/SplitResult.java
+++ src/SplitResult.java
@@ -1,2 +1,11 @@
 public class SplitResult {
+    String promotedKey;
+    Node left;
+    Node right;
+
+    SplitResult(String promotedKey, Node left, Node right) {
+        this.promotedKey = promotedKey;
+        this.left = left;
+        this.right = right;
+    }
 }
